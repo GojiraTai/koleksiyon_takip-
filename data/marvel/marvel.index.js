ecefefe9af8ca6add0519df28760ca6f9ed75748
GojiraTai <67222326+GojiraTai@users.noreply.github.com>
2026-02-03 19:25:51 +0300

    Add marvel.index.js with Marvel collections
    Created a new marvel.index.js file that exports Marvel collections and categories.

---
+++ data/marvel/marvel.index.js
@@ -0,0 +1,23 @@
+import mcu from "./mcu.collection.js";
+import mutant from "./mutant.collection.js";
+import sony from "./sony.collection.js";
+import voids from "./void.collection.js";
+import lego from "./lego.collection.js";
+import classics from "./classics.collection.js";
+import animated from "./animated.collection.js";
+import anime from "./anime.collection.js";
+
+export default {
+  title: "Marvel",
+  categories: [
+    { key:"mcu", title:"MCU" },
+    { key:"mutant", title:"Mutant Saga" },
+    { key:"sony", title:"Sony's Spider-Man Universe" },
+    { key:"void", title:"The Void" },
+    { key:"lego", title:"LEGO" },
+    { key:"classics", title:"Marvel Klasiks" },
+    { key:"animated", title:"Marvel Animated Series" },
+    { key:"anime", title:"Marvel Anime" },
+  ],
+  libs: { mcu, mutant, sony, void: voids, lego, classics, animated, anime }
+};
