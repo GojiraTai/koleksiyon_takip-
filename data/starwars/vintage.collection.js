0fafb075980e8a5ac35f63d9683f2f51fcc91050
GojiraTai <67222326+GojiraTai@users.noreply.github.com>
2026-02-03 19:33:05 +0300

    Create vintage.collection.js

---
+++ data/starwars/vintage.collection.js
@@ -0,0 +1,9 @@
+export default [
+  { id:"swv_001", title:"Star Wars Holiday Special", kind:"special" },
+  { id:"swv_002", title:"The Story of the Faithful Wookiee", kind:"short" },
+  { id:"swv_003", title:"Caravan of Courage: An Ewok Adventure", kind:"tv_movie" },
+  { id:"swv_004", title:"Ewoks: The Battle for Endor", kind:"tv_movie" },
+  { id:"swv_005", title:"Ewoks", kind:"series" },
+  { id:"swv_006", title:"Droids", kind:"series" },
+  { id:"swv_007", title:"Star Wars: Clone Wars", kind:"series" }, /* 2D micro-series OMDb'de böyle bulunuyor */
+];
