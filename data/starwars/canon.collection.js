b97d3aaa2aeffe8352daf86f0dfe261701ad6ed4
GojiraTai <67222326+GojiraTai@users.noreply.github.com>
2026-02-03 19:31:37 +0300

    Add Star Wars canon collection data

---
+++ data/starwars/canon.collection.js
@@ -0,0 +1,34 @@
+export default [
+  { id:"swc_001", title:"The Acolyte", kind:"series" },
+
+  { id:"swc_002", title:"Star Wars: Episode I - The Phantom Menace", kind:"movie" },
+  { id:"swc_003", title:"Star Wars: Episode II - Attack of the Clones", kind:"movie" },
+
+  { id:"swc_004", title:"Star Wars: The Clone Wars", kind:"series" },
+  { id:"swc_005", title:"Star Wars: Episode III - Revenge of the Sith", kind:"movie" },
+
+  { id:"swc_006", title:"Star Wars: The Bad Batch", kind:"series" },
+  { id:"swc_007", title:"Tales of the Jedi", kind:"series" },
+  { id:"swc_008", title:"Tales of the Empire", kind:"series" },
+  { id:"swc_009", title:"Tales of the Underworld", kind:"series" },
+
+  { id:"swc_010", title:"Solo: A Star Wars Story", kind:"movie" },
+  { id:"swc_011", title:"Obi-Wan Kenobi", kind:"series" },
+  { id:"swc_012", title:"Star Wars Rebels", kind:"series" },
+  { id:"swc_013", title:"Andor", kind:"series" },
+  { id:"swc_014", title:"Rogue One: A Star Wars Story", kind:"movie" },
+
+  { id:"swc_015", title:"Star Wars: Episode IV - A New Hope", kind:"movie" },
+  { id:"swc_016", title:"Star Wars: Episode V - The Empire Strikes Back", kind:"movie" },
+  { id:"swc_017", title:"Star Wars: Episode VI - Return of the Jedi", kind:"movie" },
+
+  { id:"swc_018", title:"The Mandalorian", kind:"series" },
+  { id:"swc_019", title:"The Book of Boba Fett", kind:"series" },
+  { id:"swc_020", title:"Ahsoka", kind:"series" },
+  { id:"swc_021", title:"Skeleton Crew", kind:"series" },
+  { id:"swc_022", title:"Star Wars Resistance", kind:"series" },
+
+  { id:"swc_023", title:"Star Wars: Episode VII - The Force Awakens", kind:"movie" },
+  { id:"swc_024", title:"Star Wars: Episode VIII - The Last Jedi", kind:"movie" },
+  { id:"swc_025", title:"Star Wars: Episode IX - The Rise of Skywalker", kind:"movie" },
+];
