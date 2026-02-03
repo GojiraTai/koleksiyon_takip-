acffb21bd600869dbf6d2416d480f1e358e1a771
GojiraTai <67222326+GojiraTai@users.noreply.github.com>
2026-02-03 19:32:45 +0300

    Create noncanon.collection.js

---
+++ data/starwars/noncanon.collection.js
@@ -0,0 +1,11 @@
+export default [
+  { id:"swn_001", title:"Star Wars: Visions - Season 1", kind:"series_season", baseTitle:"Star Wars: Visions", season:1 },
+  { id:"swn_002", title:"Star Wars: Visions - Season 2", kind:"series_season", baseTitle:"Star Wars: Visions", season:2 },
+  { id:"swn_003", title:"Star Wars: Visions - Season 2", kind:"series_season", baseTitle:"Star Wars: Visions", season:3 },
+  { id:"swn_004", title:"Star Wars Blips", kind:"series" },
+  { id:"swn_005", title:"Star Wars Forces of Destiny", kind:"series" },
+  { id:"swn_006", title:"Star Wars Galaxy of Adventures", kind:"series" },
+  { id:"swn_007", title:"Star Wars Galaxy of Creatures", kind:"series" },
+  { id:"swn_008", title:"Star Wars Galactic Pals", kind:"series" },
+  { id:"swn_009", title:"Zen - Grogu and Dust Bunnies", kind:"short" },
+];
