4bd7a0e569b973817248f02081ee3569908365f6
GojiraTai <67222326+GojiraTai@users.noreply.github.com>
2026-02-03 19:27:18 +0300

    Create lego.collection.js with Marvel titles
    Added a collection of LEGO Marvel special titles.

---
+++ data/marvel/lego.collection.js
@@ -0,0 +1,13 @@
+export default [
+  { id:"m_lego_01", title:"LEGO Marvel Super Heroes: Maximum Overload", kind:"special" },
+  { id:"m_lego_02", title:"LEGO Marvel Super Heroes: Avengers Reassembled", kind:"special" },
+  { id:"m_lego_03", title:"LEGO Marvel Super Heroes – Guardians of the Galaxy: The Thanos Threat", kind:"special" },
+  { id:"m_lego_04", title:"LEGO Marvel Super Heroes – Black Panther: Trouble in Wakanda", kind:"special" },
+  { id:"m_lego_05", title:"Lego Marvel Spider-Man: Vexed by Venom", kind:"special" },
+  { id:"m_lego_06", title:"Lego Marvel Avengers: Climate Conundrum", kind:"special" },
+  { id:"m_lego_07", title:"Lego Marvel Avengers: Loki in Training", kind:"special" },
+  { id:"m_lego_08", title:"Lego Marvel Avengers: Time Twisted", kind:"special" },
+  { id:"m_lego_09", title:"LEGO Marvel Avengers: Code Red", kind:"special" },
+  { id:"m_lego_10", title:"LEGO Marvel Avengers: Mission Demolition", kind:"special" },
+  { id:"m_lego_11", title:"LEGO Marvel Avengers: Strange Tails", kind:"special" },
+];
