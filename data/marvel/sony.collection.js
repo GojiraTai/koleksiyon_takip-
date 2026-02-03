62d8726dbf187dd896db9f9fc65ed42fa67b96dc
GojiraTai <67222326+GojiraTai@users.noreply.github.com>
2026-02-03 19:30:08 +0300

    Create sony.collection.js with movie data
    Added a collection of Sony Marvel movies.

---
+++ data/marvel/sony.collection.js
@@ -0,0 +1,10 @@
+export default [
+  { id:"s_001", title:"The Amazing Spider-Man", kind:"movie" },
+  { id:"s_002", title:"The Amazing Spider-Man 2", kind:"movie" },
+  { id:"s_003", title:"Venom", kind:"movie" },
+  { id:"s_004", title:"Venom: Let There Be Carnage", kind:"movie" },
+  { id:"s_005", title:"Morbius", kind:"movie" },
+  { id:"s_006", title:"Madame Web", kind:"movie" },
+  { id:"s_007", title:"Venom: The Last Dance", kind:"movie" },
+  { id:"s_008", title:"Kraven the Hunter", kind:"movie" },
+];
