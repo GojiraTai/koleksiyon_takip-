403077821de3e2754ca7727b9103bd917a132171
GojiraTai <67222326+GojiraTai@users.noreply.github.com>
2026-02-03 19:31:09 +0300

    Create anime.collection.js

---
+++ data/marvel/anime.collection.js
@@ -0,0 +1,10 @@
+export default [
+  { id:"ma_001", title:"Iron Man", kind:"series" },
+  { id:"ma_002", title:"Wolverine", kind:"series" },
+  { id:"ma_003", title:"X-Men", kind:"series" },
+  { id:"ma_004", title:"Blade", kind:"series" },
+  { id:"ma_005", title:"Iron Man: Rise of Technovore", kind:"movie" },
+  { id:"ma_006", title:"Avengers Confidential: Black Widow & Punisher", kind:"movie" },
+  { id:"ma_007", title:"Marvel Disk Wars: The Avengers", kind:"series" },
+  { id:"ma_008", title:"Marvel Future Avengers", kind:"series" },
+];
