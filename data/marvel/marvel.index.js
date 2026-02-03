6682c95a021d3448e2c9ca4ce8b46e92f39ecf5b
GojiraTai <67222326+GojiraTai@users.noreply.github.com>
2026-02-03 22:35:23 +0300

    Update marvel.index.js

--- data/marvel/marvel.index.js
+++ data/marvel/marvel.index.js
@@ -1,34 +1,24 @@
-import mcu from "./mcu.collection.js";
-import mutant from "./mutant.collection.js";
-import sony from "./sony.collection.js";
-import voids from "./void.collection.js";
-import lego from "./lego.collection.js";
-import classics from "./classics.collection.js";
-import animated from "./animated.collection.js";
-import anime from "./anime.collection.js";
-
 export default {
   title: "Marvel",
-
   categories: [
-    { key: "mcu", title: "MCU" },
-    { key: "mutant", title: "Mutant Saga" },
-    { key: "sony", title: "Sony Spider-Man Universe" },
-    { key: "void", title: "The Void" },
-    { key: "lego", title: "LEGO" },
-    { key: "classics", title: "Marvel Classics" },
-    { key: "animated", title: "Animated Series" },
-    { key: "anime", title: "Anime" }
+    { key:"mcu", title:"MCU" },
+    { key:"mutant", title:"Mutant Saga" },
+    { key:"sony", title:"Sony's Spider-Man Universe" },
+    { key:"void", title:"The Void" },
+    { key:"lego", title:"LEGO" },
+    { key:"classics", title:"Marvel Klasiks" },
+    { key:"animated", title:"Marvel Animated Series" },
+    { key:"anime", title:"Marvel Anime" },
   ],
-
+  // ÖNEMLİ: burada "dosya adı" string olacak, import yok.
   libs: {
-    mcu,
-    mutant,
-    sony,
-    void: voids,
-    lego,
-    classics,
-    animated,
-    anime
+    mcu: "mcu.collection.js",
+    mutant: "mutant.collection.js",
+    sony: "sony.collection.js",
+    void: "void.collection.js",
+    lego: "lego.collection.js",
+    classics: "classics.collection.js",
+    animated: "animated.collection.js",
+    anime: "anime.collection.js"
   }
 };
